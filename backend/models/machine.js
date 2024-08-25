ef8f097f9f3ea74cdbd05c2082b2c82042c85fd1
icy-r <asath12882@gmail.com>
2024-08-25 20:08:55 +0530

    added machine model

---
+++ backend/models/machine.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const machineSchema = new Schema(
+    {
+        name: String,
+        id: String,
+        status: String,
+        location: String,
+        description: String,
+    },
+    {
+        timestamps: true,
+    }
+    );
+
+const Machine = mongoose.model("Machine", machineSchema);
+
+export { Machine };

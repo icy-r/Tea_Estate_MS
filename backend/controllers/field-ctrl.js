716ebaaa983e34854be282d8f8708f042c69395b
JanukshanS <sjanukshan9825@gmail.com>
2024-08-27 12:47:27 +0530

    Created Field CRUD Functions

---
+++ backend/controllers/field-ctrl.js
@@ -0,0 +1,60 @@
+import {Field} from '../models/field-model.js'
+
+async function index(req, res) {
+  try {
+    //get all fields
+    const fields = await Field.find({});
+    res.json(fields);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+}
+
+async function show(req, res) {
+  try {
+    //id_field = req.params.id
+    const field = await Field.find({id: req.params.id});
+    res.json(field);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+}
+
+async function create(req, res) {
+  try {
+    const field = new Field(req.body);
+    await field.save();
+    res.json(field);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+}
+
+async function update(req, res) {
+  try {
+
+    const field = await Field.findOne({id: req.params.id});
+
+    Object.assign(field, req.body);
+    await field.save();
+    res.json(field);
+    } catch (error) {
+      res.status(400).json({ error: error });
+    }
+}
+
+async function destroy(req, res) {
+  try {
+    const field = await Field.findOne({ id: req.params.id });
+    if (!field) {
+      return res.status(404).json({ error: 'Field not found' });
+    }
+    await field.deleteOne();
+    res.json({ message: 'Field deleted' });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error });
+    }
+}
+
+export { index, show, create, update, destroy }
--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ import './config/database.js'
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as machinesRouter } from './routes/machines.js'
+import { router as fieldRouter } from './routes/field-route.js';
 
-import { router as machinesRouter } from './routes/machines.js'
 
 
 // create the express app
@@ -29,8 +29,7 @@ app.use(formData.parse())
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/machines', machinesRouter)
-
-app.use('/api/machines', machinesRouter)
+app.use('/api/fields', fieldRouter);
 
 // handle 404 errors
 app.use(function (req, res, next) {

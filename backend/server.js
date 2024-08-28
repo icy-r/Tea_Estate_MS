d40a4cd85fb7a5085943e5e3189448d5b4f6ea9f
icy-r <asath12882@gmail.com>
2024-08-28 16:50:35 +0530

    new files has been added

--- backend/server.js
+++ backend/server.js
@@ -9,19 +9,19 @@ import formData from 'express-form-data'
 import './config/database.js'
 
 // import routes
-
 //transport-management
 import { router as vehiclesRouter } from './routes/transport-management/vehicle-route.js'
-import { router as routeRouter } from './routes/transport-management/route-Route.js'
+import { router as routeRouter } from './routes/transport-management/route-route.js'
 import { router as transportRouter } from './routes/transport-management/transport-route.js'
-//repair-management
+//user-management
 import { router as profilesRouter } from './routes/user-management/profiles-route.js'
 import { router as authRouter } from './routes/authentication/auth-route.js'
+//repair-management
 import { router as machinesRouter } from './routes/repair-management/machines-route.js'
 import { router as logsRouter } from './routes/repair-management/log-route.js'
 import { router as maintenancesRouter } from './routes/repair-management/maintenance-route.js'
 import { router as repairsRouter } from './routes/repair-management/repair-req-route.js'
-import { router as vehiclesRouter } from './routes/repair-management/vehicle-route.js'
+//product-management
 import { router as catalogRouter } from './routes/product-management/catalog-route.js'
 import { router as buyersRouter } from './routes/product-management/buyer-route.js'
 
@@ -36,19 +36,22 @@ app.use(express.json())
 app.use(formData.parse())
 
 // mount imported routes
+//user-management
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
-app.use('/api/machines', machinesRouter)
 
 //transport-management
 app.use('/api/transports', transportRouter)
 app.use('/api/routes', routeRouter)
-app.use('/api/logs', logsRouter)
-app.use('/api/maintenances', maintenancesRouter)
-app.use('/api/repairs', repairsRouter)
 app.use('/api/vehicles', vehiclesRouter)
 app.use('/api/catalog', catalogRouter)
 app.use('/api/buyers', buyersRouter)
+
+//repair-management
+app.use('/api/machines', machinesRouter)
+app.use('/api/logs', logsRouter)
+app.use('/api/maintenances', maintenancesRouter)
+app.use('/api/repairs', repairsRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {

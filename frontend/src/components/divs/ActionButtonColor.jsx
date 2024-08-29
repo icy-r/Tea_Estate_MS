27faef1ce012690ee6680ae3c7584f281df3a561
icy-r <asath12882@gmail.com>
2024-08-29 18:28:40 +0530

    added component css

--- frontend/src/components/divs/ActionButtonColor.jsx
+++ frontend/src/components/divs/ActionButtonColor.jsx
@@ -2,13 +2,14 @@ import "./ActionButtonColor.css";
 
 
 // eslint-disable-next-line react/prop-types
-const ActionButtonColor = ({ key, href, text }) => {
+const ActionButtonColor = ({ key, href, text, onclickfun }) => {
     return (
         <a
             key={key}
             href={href}
             // style={styles.button}
             className="action-button-color"
+            onClick={onclickfun ? onclickfun : undefined}
         >
             {text}
         </a>
--- frontend/src/components/divs/ActionButtonTransparent.jsx
+++ frontend/src/components/divs/ActionButtonTransparent.jsx
@@ -1,12 +1,14 @@
 import "./ActionButtonColor.css"
 
 // eslint-disable-next-line react/prop-types
-const ActionButtonTransparent = ({ key, href, text }) => {
+const ActionButtonTransparent = ({ key, href, text, onclickfun }) => {
+
     return (
         <a
             key={key}
             href={href}
             className="action-button-transparent"
+            onClick={onclickfun ? onclickfun : undefined}
         >
             {text}
         </a>

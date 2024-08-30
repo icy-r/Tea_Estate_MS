b16b0057678111f2a4897a7ea504e3b33bd3f0d5
Subodhi <it22175366@my.sliit.lk>
2024-08-30 17:15:07 +0530

    chore: Add buyer and catalog controllers and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {Route, RouterProvider, Routes} from "react-router-dom";
 import Header from "./components/navbar/Header.jsx";
+import Test from "./components/test/test.jsx";
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
         <Header />
+        <Test />
         <Routes>
             <Route path="/" element={<div>Home</div>} />
             <Route path="/about" element={<div>About</div>} />

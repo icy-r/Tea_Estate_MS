0cf329967355006db96eb1a3b4e89fd1df8ca3ff
samudithTharindaka <samupuff@gmail.com>
2024-08-31 18:07:24 +0530

    added js data for slide menu

--- frontend/src/pages/transportManagement/TransportManagementDashboard.jsx
+++ frontend/src/pages/transportManagement/TransportManagementDashboard.jsx
@@ -2,45 +2,13 @@ import React from 'react';
 import Sidebar from '@components/dashboard/Sidebar.jsx';
 import Header from '@components/dashboard/HeaderDashboard.jsx';
 import Content from '@components/dashboard/Content.jsx';
+import menuItems from './menuItems.js';
 
 const TransportManagementDashboard = () => {
 
-    const menuItems = [
-        { name: 'Home', link: '/home' },
-        { 
-            name: 'Vehicle management', 
-            subItems: [
-                { name: 'Add vehicle', link: '/add-vehicle' },
-                { name: 'Manage vehicle', link: '/manage-vehicle' },
-            ] 
-        },
-        { 
-            name: 'Route management', 
-            subItems: [
-                { name: 'Add Route', link: '/add-route', active: true },
-                { name: 'Manage Routes', link: '/manage-routes' },
-            ] 
-        },
-        { 
-            name: 'Transport management', 
-            subItems: [
-                { name: 'Schedule a routine', link: '/schedule-routine' },
-                { name: 'Manage Routines', link: '/manage-routines' },
-                { name: 'Generate Report', link: '/generate-report' },
-            ] 
-        },
-        { 
-            name: 'Distribution Management', 
-            subItems: [
-                { name: 'Manage Distributions', link: '/manage-distributions' },
-                { name: 'Delivery Complains', link: '/delivery-complains' },
-            ] 
-        },
-    ];
-
     return (
         <div className="flex h-screen">
-            <div className="w-72 h-full sticky top-0 overflow-y-auto  overflow-x-hidden">
+            <div className="w-72 h-full sticky top-0 overflow-y-auto  overflow-x-hidden hidden md:block">
                 <Sidebar menuItems={menuItems} />
             </div>
             

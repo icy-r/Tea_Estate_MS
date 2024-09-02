a48e26b617b8ba20164204a48ce0c61d9a1ae69f
icy-r <asath12882@gmail.com>
2024-09-02 08:51:51 +0530

    don't ask me what I have did.  just accept.

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Menubar from "../components/menubar/Menubar.jsx";
-// eslint-disable-next-line no-unused-vars
-import {Route, RouterProvider, Routes} from "react-router-dom";
 import Home from "../components/repair-management/pages/home.jsx";
 import FieldHome from "../components/field-management/pages/FieldHome.jsx";
 import {Route, Routes} from "react-router-dom";
@@ -13,15 +11,15 @@ function App() {
     const [open, setOpen] = useState(false);
     return (
         <>
-            <Header props={setOpen} />
-            <Menubar props={[open, setOpen]} />
+            <Header props={setOpen}/>
+            <Menubar props={[open, setOpen]}/>
 
             <Routes>
-                <Route path="/" element={<div>Home</div>} />
-                <Route path="/about" element={<div>About</div>} />
-                <Route path="/repair/*" element={<Home />} />
-                <Route path="/field/*" element={<FieldHome />} />
-                <Route path="/repair/*" element={<RepairRoutes />} />
+                <Route path="/" element={<div>Home</div>}/>
+                <Route path="/about" element={<div>About</div>}/>
+                <Route path="/repair/*" element={<Home/>}/>
+                <Route path="/field/*" element={<FieldHome/>}/>
+                <Route path="/repair/*" element={<RepairRoutes/>}/>
             </Routes>
         </>
     )

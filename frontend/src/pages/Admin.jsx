36e852e630e3daac1183a195a74db1bb902e6680
icy-r <asath12882@gmail.com>
2024-09-05 20:26:32 +0530

    some non-sense routes are cleared, workin on some more functionalities

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
-import { Route, Routes } from "react-router-dom";
-//import components
-import Error404 from './error404.jsx';
+import {useState} from 'react';
 import Home from "../components/repair-management/pages/home.jsx";
 import FieldHome from "../components/field-management/pages/FieldHome.jsx";
 import TransportHome from '../components/transport-management/pages/TransportHome.jsx';
@@ -12,6 +9,8 @@ import Content from '../components/dashboard/Content.jsx';
 import menuItems from './menuItems.js';
 
 import '../App.css';
+import Error404 from "./error404.jsx";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -24,24 +23,25 @@ function App() {
         <>
             <div className="flex h-screen">
                 {isSidebarOpen &&
-                <div className={`w-72 h-full sticky top-0 overflow-y-auto overflow-x-hidden ease-in transition duration-700 md:block`}>
-                <Sidebar menuItems={menuItems} />
-            </div>}
+                    <div
+                        className={`w-72 h-full sticky top-0 overflow-y-auto overflow-x-hidden ease-in transition duration-700 md:block`}>
+                        <Sidebar menuItems={menuItems}/>
+                    </div>}
 
                 <div className="flex flex-col w-full h-full overflow-y-auto">
                     <div className="bg-white sticky top-0 z-10">
-                        <Header mainTitle="Route Management" subTitle="Adding New Route" toggleSidebar={toggleSidebar} />
+                        <Header mainTitle="Route Management" subTitle="Adding New Route" toggleSidebar={toggleSidebar}/>
                     </div>
 
                     <Content>
                         <Routes>
-                            <Route path="/" element={<div>Home</div>} />
-                            <Route path="/about" element={<div>About</div>} />
-                            <Route path="/repair/*" element={<Home />} />
-                            <Route path="/field/*" element={<FieldHome />} />
-                            <Route path="/repair/*" element={<RepairRoutes />} />
-                            <Route path="/transport/*" element={<TransportHome />} />
-                            <Route path="/*" element={<Error404 />} />
+                            <Route path="/" element={<div>Home</div>}/>
+                            <Route path="/about" element={<div>About</div>}/>
+                            <Route path="/repair/*" element={<Home/>}/>
+                            <Route path="/field/*" element={<FieldHome/>}/>
+                            <Route path="/repair/*" element={<RepairRoutes/>}/>
+                            <Route path="/transport/*" element={<TransportHome/>}/>
+                            <Route path="/*" element={<Error404/>}/>
                         </Routes>
                     </Content>
                 </div>
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,38 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Admin from "./pages/Admin.jsx";
 import LandingPage from "./pages/landingPage/LandingPage.jsx";
 import Error404 from "./pages/error404.jsx";
-
 import Login from "./components/product-management/Login.jsx";
-import Marketplace from "./pages/productManagement/marketPlace.jsx";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <LandingPage />
+        element: <LandingPage/>
     },
     {
         path: "/admin/*",
-        element: <Admin />,
-    },
-    {
-        path: "/marketplace/*",
-        element: <Marketplace />,
+        element: <Admin/>,
     },
     {
         path: "/login",
-        element: <Login />,
+        element: <Login/>,
     },
-    {
-        path: "/error404",
-        element: <Error404/>,
-    },
-   
     {
         path: "*",
-        element: <Error404 />,
-    }
+        element: <Error404/>,
+    },
 ]);
 
 export default function Routes() {

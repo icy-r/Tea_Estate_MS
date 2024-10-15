a62a45dadff91d62299de1e59c5bab5b6e327e0b
JanukshanS <sjanukshan9825@gmail.com>
2024-10-16 03:59:40 +0530

    For One Last Time

--- frontend/src/components/field-management/pages/FieldHome.jsx
+++ frontend/src/components/field-management/pages/FieldHome.jsx
@@ -3,16 +3,21 @@ import StatusMain from "../component/StatusMain";
 import ErrorBoundary from "../../ErrorBoundary";
 import Chart from "../component/Chart";
 import BarChart from "../component/BarChart";
+import UpcomingTable from "../component/UpcomingTable";
 
 const FieldHome = () => {
   return (
     <ErrorBoundary>
       <StatusMain />
+      <div style={{ margin: "20px 0" }}>
+        <UpcomingTable />
+      </div>
       <div
         style={{
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
+          margin: "20px 0",
         }}
       >
         <div style={{ flex: 1, paddingRight: "10px" }}>

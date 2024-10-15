2331d23d48c231fa057cc50fe45773abc2b210d4
samudithTharindaka <samupuff@gmail.com>
2024-10-15 13:35:05 +0530

    finishing distribution

---
+++ backend/models/sales-management/order-model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+    {
+        orderID: String,
+        orderDate: Date,
+        quantity: String,
+        pid: String,
+        buyer_id: String, 
+        status: String,
+        transportation_id: String,
+        
+    },
+
+    {
+        timestamps: true,
+    }
+    );
+
+
+const Order = mongoose.model("orders", orderSchema);
+
+export { Order };
